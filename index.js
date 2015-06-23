f610173fab5ad8615c16d13a715fc78fc7a1a4bc
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-06-23 19:11:55 +0100

    Initial /convert resource to convert text.

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-var http = require('http');
+var server = require('./lib/server');
 
-var port = process.argv[2] || 8080; 
-
-function handleRequest(request, response) {
-  response.end('It Works!! Path Hit: ' + request.url);
-}
-
-var server = http.createServer(handleRequest);
-
-server.listen(port, function() {
-  console.log("Server listening on: http://localhost:%s", port);
-});
+server.listen(process.argv[2] || 8080);
 
---
+++ tests/indexSpec.js
@@ -0,0 +1,33 @@
+var assert = require('assert'),
+    request = require("request");
+ 
+var server = require('../lib/server');
+var port = process.argv[3] || 8080;
+
+describe('server', function() {
+  before(function() {
+    server.listen(port);
+  });
+
+  after(function () {
+    server.close();
+  });
+
+  describe('/convert', function () {
+    it('should convert markdown to mediawiki', function(done) {
+      var params = {
+        uri: 'http://localhost:' + port + '/convert',
+        method: "POST",
+        from: 'markdown',
+        to: 'html',
+        text: '# Heading'
+      };
+      
+      request(params, function(error, response, body) {
+        assert.equal('= Heading =', body.trim());
+        done();
+      });
+    });
+  });
+});
+
