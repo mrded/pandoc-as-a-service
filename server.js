2edb627f3f608aff68e93c2ee47949c6bb7db3d7
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-07-15 00:31:50 +0100

    Refactor server.

--- server.js
+++ server.js
@@ -5,21 +5,26 @@ var express = require('express'),
 this.server = express();
 this.server.set('views', './views');
 this.server.set('view engine', 'ejs');
+this.server.use(express.static('public'));
 
 this.server.get('/', function(req, res) {
   res.render('index');
 });
 
 this.server.post('/:format', function(req, res) {
-  var to = req.params.format;
-  var from = req.get('Content-Type').split("/")[1];
-
-  helpers.getBody(req, function(body) {
-    pdc(body, from, to, function(err, result) {
-      if (err) res.sendStatus(400)
-        else res.append('Content-Type', 'text/' + to).send(result);
-    });
-  });
+  var contentType = req.get('Content-Type');
+  
+  if (contentType) {
+    var from = contentType.split("/")[1];
+    var to = req.params.format;
+  
+    helpers.getBody(req, function(body) {
+      pdc(body, from, to, function(err, result) {
+        if (err) res.sendStatus(400)
+          else res.append('Content-Type', 'text/' + to).send(result);
+      });
+    });  
+  } else res.sendStatus(400) 
 });
 
 exports.listen = function(port) {
